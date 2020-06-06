bf502baf708adb1f5ab27c2033a9f1dbedbd7c3c
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-06 15:09:17 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,24 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(e) {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $(window).on('load', function(e) {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+      console.log(e)
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
+getIt()
+frameIt()
+pressIt()
 
 });
